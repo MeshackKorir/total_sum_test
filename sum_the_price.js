9433fbc9fad2beac1a000582237110c660b32c5d
MeshackKorir <meshackkorir100@gmail.com>
2024-01-16 08:30:53 +0300

    chaining methods

--- sum_the_price.js
+++ sum_the_price.js
@@ -1,4 +1,12 @@
 //Give me the sum of the prices (try chaining methods) [{price: 10.99}, {price: 5.99}, {price: 29.99}]
-const prices =[
-    {price: 10.99}, {price: 5.99}, {price: 29.99}
-]
+const items = [
+    {price: 10.99},
+    {price: 5.99},
+    {price: 29.99}
+];
+
+const total = items
+    .map(item => item.price)
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+console.log(total);    
